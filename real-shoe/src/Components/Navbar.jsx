dbc5bf7fd85b727f0099b5f1e87b22beb62982af
Chanuka Sandaruwan Warnasooriya <warnasooriyacs1000@gmail.com>
2024-02-18 00:01:36 +0530

    Navbar Update

--- real-shoe/src/Components/Navbar.jsx
+++ real-shoe/src/Components/Navbar.jsx
@@ -1,10 +1,61 @@
-import React from 'react'
+import { useState } from "react"
+import { IoMdCloseCircleOutline } from "react-icons/io";
+import { IoMenu } from "react-icons/io5";
+
 
 const Navbar = () => {
+
+    const [dropdown, setDropdown] = useState(false);
+
+    const showDropdown = () =>{
+        setDropdown(!dropdown);
+    }
   return (
-    <div>
+    <nav>
+        <div>
+            <div>
+                <div>
+                    <span>
+                        <img src="/logo-black.png" alt="logo image" />
+                    </span>
+                </div>
+
+                <ul>
+                    <a href="#home">Home</a>
+                    <a href="#home">About</a>
+                    <a href="#home">Colors</a>
+                    <a href="#home">Features</a>
+                    <a href="#home">Cart</a>
+                </ul>
+
+                {dropdown ? (
+                    <div onClick={showDropdown}>
+                        <IoMdCloseCircleOutline/>
+                    </div> 
+                ) : (
+                    <div onClick={showDropdown}>
+                        <IoMenu/>
+                    </div> 
+                )}
+            </div>
+
+            {dropdown ? (
+                    <div onClick={showDropdown}>
+                        <div>
+                            <ul>
+                                <a href="#home">Home</a>
+                                <a href="#home">About</a>
+                                <a href="#home">Colors</a>
+                                <a href="#home">Features</a>
+                                <a href="#home">Cart</a>
+                            </ul>
+                        </div>
+                    </div> 
+                ) : null
+                }
+        </div>
       
-    </div>
+    </nav>
   )
 }
 
